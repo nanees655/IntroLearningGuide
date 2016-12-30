07173cdddf5b2a7d1df364afae74bae8cdd0e3be
Nadia Anees <n.anees655@gmail.com>
2016-12-30 14:16:47 -0800

    This commit will add Problem7

---
+++ All Java Work/src/Problem7.java
@@ -0,0 +1,21 @@
+
+public class Problem7 {
+	public static void main(String[] args) {
+	
+//converting days into seconds
+	
+	int day = 24;
+	int hour = 60;
+	int minute = 60;
+	
+	
+	int totalSeconds = day * hour * minute;
+	
+ System.out.println("In ten days there are");
+ System.out.println(totalSeconds);
+ System.out.println("seconds.");
+	
+	}
+
+	
+}
